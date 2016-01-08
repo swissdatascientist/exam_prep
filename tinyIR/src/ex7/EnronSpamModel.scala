977b841d6918a25e9e707b8f483983e9832e2712
swissdatascientist <mfuhr@gmx.ch>
2016-01-08 15:52:55 +0100

    initial setup enron spam model.

---
+++ tinyIR/src/ex7/EnronSpamModel.scala
@@ -0,0 +1,15 @@
+package ex7
+
+/**
+  * Created by mafu on 08.01.2016.
+  */
+class EnronSpamModel {
+
+}
+
+object EnronSpamModel {
+  def main(args: Array[String]): Unit = {
+    println("Exercise 7 - Enron Spam Model")
+
+  }
+}
